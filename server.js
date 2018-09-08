e6c8b688205d78f7fdda27696053e4872a88ec52
Ann Schulte <aschulte1919@gmail.com>
2018-09-08 12:35:00 -0600

    changes made to auth protocal

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-// Starting the server, syncing our models ------------------------------------/
+// Starting the server, syncing our models //
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
     console.log(
@@ -35,5 +35,45 @@ db.sequelize.sync(syncOptions).then(function() {
   });
 });
 
+
+
 module.exports = app;
 
+
+
+
+
+function login(email, password, callback) {
+  var connection = mysql({
+    host: 'localhost',
+    user: 'root',
+    password: 'secret',
+    database: 'mydb'
+  });
+
+  connection.connect();
+
+  var query = "SELECT id, nickname, email, password " +
+    "FROM users WHERE email = ?";
+
+  connection.query(query, [email], function (err, results) {
+    if (err) return callback(err);
+    if (results.length === 0) return callback(new WrongUsernameOrPasswordError(email));
+    var user = results[0];
+
+    bcrypt.compare(password, user.password, function (err, isValid) {
+      if (err) {
+        callback(err);
+      } else if (!isValid) {
+        callback(new WrongUsernameOrPasswordError(email));
+      } else {
+        callback(null, {
+          id: user.id.toString(),
+          nickname: user.nickname,
+          email: user.email
+        });
+      }
+    });
+
+  });
+}
