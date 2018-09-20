0894782ecaf40ac5e056fa3d566efcb555a23f41
Ann Schulte <aschulte1919@gmail.com>
2018-09-20 13:47:50 -0600

    heroku try

--- scripts/create_database.js
+++ scripts/create_database.js
@@ -1,20 +1,34 @@
 var mysql = require('mysql');
 var dbconfig = require('../config/database');
 
-var connection = mysql.createConnection(dbconfig.connection);
+var connection;
 
-connection.query('CREATE DATABASE ' + dbconfig.database);
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection(dbconfig.connection);
+}
+connection.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    };
 
-connection.query('\
-CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.users_table + '` ( \
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
-    `username` VARCHAR(20) NOT NULL, \
-    `password` CHAR(60) NOT NULL, \
-        PRIMARY KEY (`id`), \
-    UNIQUE INDEX `id_UNIQUE` (`id` ASC), \
-    UNIQUE INDEX `username_UNIQUE` (`username` ASC) \
-)');
+    console.log('connected as id ' + connection.threadId);
+});
 
-console.log('Success: Database Created!')
+// connection.query('CREATE DATABASE ' + dbconfig.database);
 
-connection.end();
+// connection.query('\
+// CREATE TABLE `' + dbconfig.database + '`.`' + dbconfig.users_table + '` ( \
+//     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, \
+//     `username` VARCHAR(20) NOT NULL, \
+//     `password` CHAR(60) NOT NULL, \
+//         PRIMARY KEY (`id`), \
+//     UNIQUE INDEX `id_UNIQUE` (`id` ASC), \
+//     UNIQUE INDEX `username_UNIQUE` (`username` ASC) \
+// )');
+
+// console.log('Success: Database Created!')
+
+// connection.end();
